070c2085a40aa0197d98a3eeec87ae83a1da9df6
IngJahir <jahirtautiva@gmail.com>
2021-07-28 02:19:41 -0500

    Nueva version

--- VISTAS/NAVIGATOR_1.php
+++
@@ -1,6 +0,0 @@
-<nav>
-    <ul>
-        <li> <a href="../../INDEX.php"> HOME </a> </li>
-        <li> <a href="../../INDEX.php #software_Developments"> PROJECTS </a> </li>
-    </ul>
-</nav>
--- VISTAS/NAVIGATOR_2.php
+++
@@ -1,6 +0,0 @@
-<nav>
-    <ul>
-        <li> <a href="../../INDEX.php"> HOME </a> </li>
-        <li> <a href="../../INDEX.php #simulation_FEA_HPC"> SIMULATIONS </a> </li>
-    </ul>
-</nav>
